83c53fda5d7e077ff3767780bf3bdeec4394e8e3
SABUYA, Marvic N. <165488772+dexluna@users.noreply.github.com>
2025-02-19 19:50:22 +0800

    Update program2.cpp

--- program2.cpp
+++ program2.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    double diameter;
+    cout << "Enter diameter: ";
+    cin >> diameter;
+    double radius = diameter / 2;
+    double area = 3.14159 * radius * radius;
+    cout << "Area of circle: " << area;
+    return 0;
+}
